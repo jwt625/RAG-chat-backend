801988a9f1f82b753411f9dc17f8a18485d36285
Wentao <jwt625@gmail.com>
2025-07-24 06:40:33 +0000

    update config

--- app/config.py
+++ app/config.py
@@ -41,6 +41,7 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
         case_sensitive = True
+        extra = "ignore"  # Ignore extra fields in .env file
 
 
 @lru_cache()
